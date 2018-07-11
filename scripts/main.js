d0605be4716d2a3b6b873e7d2e2ee7f246c174ef
craig <grendel@shmoo.com>
2018-07-11 14:37:05 -0700

    Added some folders

---
+++ scripts/main.js
@@ -0,0 +1,17 @@
+const axios = require('axios')
+const templates = require('./templates.js')
+
+
+// Navbar
+
+// Login
+
+// Register
+
+// All Lists 
+
+// Create a new Task
+
+// Doing
+
+// Done
