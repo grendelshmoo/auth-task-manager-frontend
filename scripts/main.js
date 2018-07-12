cc83b0cad1f42440aea5609fd82971d661c1466c
craig <grendel@shmoo.com>
2018-07-12 09:50:10 -0700

    Added DOM targets

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,11 @@
 const axios = require('axios')
 const apiUrl = `http://localhost:5000/api/`
+
+//DOM Targets
 const centerColumn = document.getElementById('center-column')
 const leftColumn = document.getElementById('left-column')
 const rightColumn = document.getElementById('right-column')
+const navBar = document.getElementById('navbar-select')
 
 // Navbar
 
