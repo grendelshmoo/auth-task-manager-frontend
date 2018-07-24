6ca57a893c618afdc0d156ba4d800a251c3f2a3e
dtruong11 <dtruong11@users.noreply.github.com>
2018-07-24 16:10:44 -0700

    Rendering from doing to done

--- scripts/render/render-tasklist.js
+++ scripts/render/render-tasklist.js
@@ -7,20 +7,23 @@ const leftColumn = document.getElementById('left-column')
 const rightColumn = document.getElementById('right-column')
 const templates = require('../templates/lists')
 
+const completeTask = require('./task-lists-success').completeTask
+
+
 function renderTaskList(lists) {
   // set the navbar of task list 
   const navSelect = document.getElementById('navbar-select')
   navSelect.innerHTML = taskmenuTemplate()
-  
+
   const logoutSelect = document.getElementById('logout-link')
   logoutSelect.addEventListener('click', logOut)
-  
+
   // const loginLink = document.getElementById('login-link')
   leftColumn.innerHTML = tasklistTemplate.left()
   centerColumn.innerHTML = tasklistTemplate.center()
   rightColumn.innerHTML = tasklistTemplate.right()
   populateDoingDone(lists[0])
-  console.log('I am lists[0], yayyy',lists[0])
+  console.log('I am lists[0], yayyy', lists[0])
 }
 
 function logOut() {
@@ -65,7 +68,6 @@ function renderTasksById(lists) {
         }
       })
     })
-
   })
 }
 
@@ -73,27 +75,56 @@ function populateDoingDone(list) {
   const tasks = list.tasks
   tasks.map(task => {
     console.log('Iam a  task, look at me', task)
-      if (!task.completed) {
-      const singleCard = tasklistTemplate.doingCards(task.title, task.description, task.created_at)
+    if (!task.completed) {
+      const singleCard = tasklistTemplate.doingCards(task.id, task.title, task.description, task.list_id, task.id, task.created_at)
       centerColumn.innerHTML += singleCard
     } else {
-      const singleCard = tasklistTemplate.completedCards(task.title, task.description, task.updated_at)
+      const singleCard = tasklistTemplate.completedCards(task.id, task.title, task.description, task.updated_at)
       rightColumn.innerHTML += singleCard
     }
   })
+  // moving completed cards from doing to done
+  movingDoingToDone()
 }
 
-function renderPopulateLists (lists) {
+function movingDoingToDone() {
+  const completeBtns = Array.from(document.querySelectorAll(".complete"))
+  completeBtns.map(btn => {
+    btn.addEventListener('click', function (event) {
+      const taskId = event.target.dataset.taskid
+      const listId = event.target.dataset.listid
+      const token = localStorage.getItem('token')
+      completeTask(listId, taskId, token)
+    })
+  })
+}
+
+
+// function completeTask(listId, taskId) {
+//   const body = {
+//     'completed': true
+//   }
+//   return axios.patch(`https://atm-server-g92.herokuapp.com/api/lists/${listId}/tasks/${taskId}`, body, {
+//     headers: {
+//       authorization: `Bearer ${localStorage.getItem('token')}`
+//     }
+//   }).then(response => {
+//     // render the whole task page again. 
+//     getTasks()
+//   })
+// }
+
+function renderPopulateLists(lists) {
   renderListsGroupItems(lists)
   renderTasksById(lists)
 }
 
 module.exports = {
-
   renderListsGroupItems,
   renderTasksById,
   renderTaskList,
   populateDoingDone,
-  renderPopulateLists
+  renderPopulateLists,
+  movingDoingToDone,
+  completeTask
 }
-
