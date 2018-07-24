221f3f6c8eb6f67c160a3e3ccd09969cb95e42e0
dtruong11 <dtruong11@users.noreply.github.com>
2018-07-23 17:09:44 -0700

    trying to render

--- scripts/templates/lists.js
+++ scripts/templates/lists.js
@@ -2,4 +2,6 @@ function listTemplate(list) {
   return `<a id="list-select" class="list-group-item list-group-item-action" data-id="${list.id}" data-toggle="list" href="#${list.id}" role="tab">${list.title}</a>`
 }
 
-module.exports = { listTemplate }
+module.exports = {
+  listTemplate
+}
